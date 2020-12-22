853e4c5497ef91be92981c3cc0ec9603bb2250a1
Jithu <45289711+discodancer212@users.noreply.github.com>
2020-12-22 21:24:02 +0530

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,54 @@
+import requests, time
+import urlib
+import webbrowser
+from colorama import Fore
+
+print(f'''\n
+            
+                             
+                                ██████╗░██╗░██████╗░█████╗░░█████╗░
+                                ██╔══██╗██║██╔════╝██╔══██╗██╔══██╗
+                                ██║░░██║██║╚█████╗░██║░░╚═╝██║░░██║   AUTHOR{Fore.RESET}: "discodancer212"
+                                ██║░░██║██║░╚═══██╗██║░░██╗██║░░██║
+                                ██████╔╝██║██████╔╝╚█████╔╝╚█████╔╝   VERSION{Fore.RESET}: 1.0
+                                    VERSION{Fore.RESET}: 1.0
+        ═══════════════════════════════════════════════════════════════════
+        
+        ═══════════════════════════════════════════════════════════════════''')
+
+		
+		
+		
+		
+		import webbrowser
+webbrowser.open_new_tab("https://pastebin.com/raw/JZ58sk9T/")
+
+		
+print(f"[{Fore.GREEN}>{Fore.RESET}] Webhook link ")
+link = input(" > ")
+print(f"[{Fore.GREEN}>{Fore.RESET}] Message to send ")
+message = input(" > ")
+print(f"[{Fore.GREEN}>{Fore.RESET}] Webhook name ")
+name = input(" > ")
+
+payload = {
+  'content': message,
+  'username': name
+}
+
+while True:
+  try:
+    time.sleep(0.5)
+    r = requests.post(link, json=payload)
+    f = open('log.txt', 'a+') 
+    if 'You are being rate limited.' in r.text:
+      f.write('Ratelimited\n')
+    else:
+      f.write(f"Spammed {message}\n")  
+    f.close()
+    if r.status_code == 204:
+      print(f"[{Fore.GREEN}+{Fore.RESET}] Sent message!")
+    else:
+      print(f"[{Fore.RED}-{Fore.RESET}] Ratelimit/Error")
+  except requests.exceptions.MissingSchema:
+    print(f"[{Fore.RED}-{Fore.RESET}] Not a real url");break
